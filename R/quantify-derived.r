c8f0406aee668f12cfbf8c73177f4f1501e7c113
bmihaljevic <boki.mihaljevic@gmail.com>
2018-12-04 17:36:56 +0100

    updates

--- R/quantify-derived.r
+++ R/quantify-derived.r
@@ -13,6 +13,7 @@ compute_derived <- function(cell_vars) {
   newvars[['ratio_y']] <- ratio(cell_vars, 'height' , 'y_sd')
   newvars[['density_bifs']] <- ratio(cell_vars, 'N_bifurcations' , 'total_length')
   newvars[['density_dist']] <-  ratio(cell_vars, 'node_root_path.max' , 'total_length')
+  newvars[['aspect_ratio']] <-  ratio(cell_vars, 'dims.PC1' , 'dims.PC2')
   # not currently computed i think
   # newvars[['density_area']] <-  ratio(cell_vars, 'grid_area' , 'total_length')
   newvars[['tree_length.avg']] <- ratio(cell_vars, 'total_length' , 'N_stems')
